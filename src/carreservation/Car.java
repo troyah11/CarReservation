09bc574c8237cd2e7ded4a11b19a823edb623722
troyah11 <trasonaegietroy.aws@gmail.com>
2023-09-20 14:21:20 +0800

    initial commit

--- src/carreservation/Car.java
+++ src/carreservation/Car.java
@@ -3,4 +3,16 @@ package carreservation;
 public class Car {
 	String color;
 	String brand;
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
 }
